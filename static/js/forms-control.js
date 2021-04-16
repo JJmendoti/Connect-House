c51cbd61a15b7ade823fb7bf89eb519c90e95259
Jhon Mendoza <jmendoti.cesde@gmail.com>
2021-04-16 17:19:04 -0500

    Jhon - Se crea carousel para index con estilos y funcion en form-control

--- static/js/forms-control.js
+++ static/js/forms-control.js
@@ -1,36 +1,59 @@
-$(document).ready(function(){
-
-    $('#go-propery').click(function(){
-          $('.form-user').hide();
-          $('.form-property').show();
-
-    });
-
-    $('#go-user').click(function(){
-        $('.form-user').show();
-        $('.form-property').hide();
-
+$(document).ready(function () {
+  $("#go-propery").click(function () {
+    $(".form-user").hide();
+    $(".form-property").show();
   });
 
+  $("#go-user").click(function () {
+    $(".form-user").show();
+    $(".form-property").hide();
+  });
 
-    $('#go-property-register').click(function(){
-        $('.form-register-user').hide();
-        $('.form-register-property').show();
+  $("#go-property-register").click(function () {
+    $(".form-register-user").hide();
+    $(".form-register-property").show();
+  });
 
-    });
+  $("#go-user-register").click(function () {
+    $(".form-register-user").show();
+    $(".form-register-property").hide();
+  });
+});
 
-    $('#go-user-register').click(function(){
-        $('.form-register-user').show();
-        $('.form-register-property').hide();
+$("li a").click(function (e) {
+  //e.preventDefault();
+  let $this = $(this);
+  $this.closest("ul").find("li.active,a.active").removeClass("active");
+  $this.addClass("active");
+  $this.parent().addClass("active");
+});
 
-    });
- });
-
- $('li a').click(function(e) {
-    //e.preventDefault();
-    let $this = $(this);
-    $this.closest('ul').find('li.active,a.active').removeClass('active');
-    $this.addClass('active');
-    $this.parent().addClass('active');
-
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
 });
