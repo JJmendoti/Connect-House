6c2dd5cc68e3bb776adc38ace280ad3ee3af16c2
marvaibanc <marvaicolombia@gmail.com>
2021-04-15 21:00:36 -0500

    Yohan, se agregan cambios en app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
+import pymongo
+from bson import ObjectId
+import os
+from werkzeug.utils import secure_filename
+
+#se crea copnexion a base datos
+myClient = pymongo.MongoClient("mongodb://localhost:27017")
+#se crea base de datos
+myDB = myClient["rentApp"]
+#se crean colecciones
+apartmentsCollection = myDB["apartments"]
+onwerCollection = myDB["onwer"]
+userCollection = myDB["user"]
 
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = './static/img/uploads'
 
 @app.route('/')
 def index():
@@ -30,147 +41,237 @@ def homeonwer():
     return render_template('homeOnwer.html')
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/rentApp'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
-
-class User(db.Model):
-    iduser = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100))
-    ident = db.Column(db.String(15))
-    email = db.Column(db.String(50))
-    country = db.Column(db.String(50))
-    city = db.Column(db.String(50))
-    password = db.Column(db.String(15))
-
-    def __init__(self, name, ident, email, country, city, password):
-        self.name = name
-        self.ident = ident
-        self.email = email
-        self.country = country
-        self.city = city
-        self.password = password
-
-class Onwer(db.Model):
-    idonwer = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100))
-    ident = db.Column(db.String(15))
-    email = db.Column(db.String(50))
-    country = db.Column(db.String(50))
-    city = db.Column(db.String(50))
-    password = db.Column(db.String(15))
-
-    def __init__(self, name, ident, email, country, city, password):
-        self.name = name
-        self.ident = ident
-        self.email = email
-        self.country = country
-        self.city = city
-        self.password = password
-
-class Apartment(db.Model):
-    idapartment = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100))
-    city = db.Column(db.String(100))
-    address = db.Column(db.String(100))
-    location = db.Column(db.String(255))
-    country = db.Column(db.String(50))
-    image = db.Column(db.String(200))
-    image_featured = db.Column(db.String(200))
-    nigth_value = db.Column(db.Integer)
-    review = db.Column(db.Text)
-
-
-    def __init__(self, name, city, address, location, country, image, image_featured,nigth_value,review):
-        self.name = name
-        self.city = city
-        self.address = address
-        self.country = country
-        self.location = location
-        self.image = image
-        self.image_featured = image_featured
-        self.nigth_value = nigth_value
-        self.review = review
-
-
-db.create_all()
-
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ('name','ident', 'email','country','city','password')
-
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
-
 @app.route('/user/', methods=['GET'])
 def user():
-    all_user = User.query.all()
-    result = users_schema.dump(all_user)
-    return jsonify(result)
-
+    result = userCollection.find()
+    if result:
+        return render_template("allUser.html", data=result)
+    else:
+        return render_template("404.html")
 
 @app.route('/user/<id>',methods=['GET'])
 def user_by_id(id):
-        user = User.query.get(id)
-        if user:
-            return user_schema.jsonify(user)
-        else:
-            return jsonify({'status':'404'})
+        query = {"_id": ObjectId(id)}
+        result = userCollection.find_one(query)
+        if result:
+            return render_template("homeUser.html", data = result)
+        else: 
+            return render_template("404.html")
 
-
-@app.route('/user/<id>',methods=['DELETE'])
-def delete_user(id):
-    try:  
-        user = User.query.get(id)
-        db.session.delete(user)
-        db.session.commit()
-        return jsonify({'status':'200'})
-    except:
-        return jsonify({'status':'Error, puede que el usuario no exista o ya haya sido eliminado'})
+# @app.route('/user/<id>',methods=['DELETE'])
+# def delete_user(id):
+#     try:  
+#         user = User.query.get(id)
+#         db.session.delete(user)
+#         db.session.commit()
+#         return jsonify({'status':'200'})
+#     except:
+#         return jsonify({'status':'404'})
 
 @app.route('/user', methods=['POST'])
 def send_user():
-    data = request.get_json(force=True)
-    name = data['name']
-    ident = data['ident']
-    email = data['email']
-    country = data['country']
-    city = data['city']
-    password = data['password']
-
-    regist = User(name,ident,email,country,city,password)
-    db.session.add(regist)
-    db.session.commit()
-    return jsonify({'status':'Usuario registrado correctamente'})
-
-@app.route('/user/<id>', methods=['PUT'])
-def up_user(id):
-    try:
-        user = User.query.get(id)
-        data = request.get_json(force=True)
-        name = data['name']
-        ident = data['ident']
-        email = data['email']
-        country = data['country']
-        city = data['city']
-        password = data['password']
-
-        user.name = name
-        user.ident = ident
-        user.email = email
-        user.country = country
-        user.city = city
-        user.password = password
-        db.session.commit()
+    name = request.form.get('name')
+    ident =request.form.get('ident')
+    email = request.form.get('email')
+    country = request.form.get('country')
+    f = request.files['avatar']
+    filename = secure_filename(f.filename)
+    f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    city = request.form.get('city')
+    password = request.form.get('password')
+    user = {"name": name, "ident":ident, "email": email, "avatar": filename, "country": country, "city": city, "password":password}
+    save = userCollection.insert_one(user)
+    if save:
         return jsonify({'status':'200'})
-    except:
-        return jsonify({'status':'El registro no existe'})
+    else:
+       return jsonify({'status':'404'})
 
+# @app.route('/user/<id>', methods=['PUT'])
+# def up_user(id):
+#     try:
+#         user = User.query.get(id)
+#         data = request.get_json(force=True)
+#         name = data['name']
+#         ident = data['ident']
+#         email = data['email']
+#         country = data['country']
+#         city = data['city']
+#         password = data['password']
+
+#         user.name = name
+#         user.ident = ident
+#         user.email = email
+#         user.country = country
+#         user.city = city
+#         user.password = password
+#         db.session.commit()
+#         return jsonify({'status':'200'})
+#     except:
+#         return jsonify({'status':'404'})
+
+
+# #esquema y rutas de propietarios
+
+
+
+# @app.route('/onwer/', methods=['GET'])
+# def onwer():
+#     all_onwer = Onwer.query.all()
+#     result = onwers_schema.dump(all_onwer)
+#     return jsonify(result)
+
+
+# @app.route('/onwer/<id>',methods=['GET'])
+# def onwer_by_id(id):
+#         onwer = Onwer.query.get(id)
+#         if onwer:
+#             return onwer_schema.jsonify(onwer)
+#         else:
+#             return jsonify({'status':'404'})
+
+
+# @app.route('/onwer/<id>',methods=['DELETE'])
+# def delete_onwer(id):
+#     try:  
+#         onwer = Onwer.query.get(id)
+#         db.session.delete(onwer)
+#         db.session.commit()
+#         return jsonify({'status':'200'})
+#     except:
+#         return jsonify({'status':'404'})
+
+# @app.route('/onwer', methods=['POST'])
+# def send_onwer():
+#     data = request.get_json(force=True)
+#     name = data['name']
+#     ident = data['ident']
+#     email = data['email']
+#     country = data['country']
+#     city = data['city']
+#     password = data['password']
+
+#     regist = Onwer(name,ident,email,country,city,password)
+#     db.session.add(regist)
+#     db.session.commit()
+#     return jsonify({'status':'200'})
+
+# @app.route('/onwer/<id>', methods=['PUT'])
+# def up_onwer(id):
+#     try:
+#         onwer = Onwer.query.get(id)
+#         data = request.get_json(force=True)
+#         name = data['name']
+#         ident = data['ident']
+#         email = data['email']
+#         country = data['country']
+#         city = data['city']
+#         password = data['password']
+
+#         onwer.name = name
+#         onwer.ident = ident
+#         onwer.email = email
+#         onwer.country = country
+#         onwer.city = city
+#         onwer.password = password
+#         db.session.commit()
+#         return jsonify({'status':'200'})
+#     except:
+#         return jsonify({'status':'404'})
+
+# #esquema y rutas de apartamentos
+
+
+# @app.route('/apartment/', methods=['GET'])
+# def apartment():
+#     all_apartment = Onwer.query.all()
+#     result = apartments_schema.dump(all_apartment)
+#     if result:
+#         return jsonify(result)
+#     else:
+#         return jsonify({'status':'404'})
+
+
+# @app.route('/apartment/<id>',methods=['GET'])
+# def apartment_by_id(id):
+#         apartment = Apartment.query.get(id)
+#         if apartment:
+#             return apartment_schema.jsonify(apartment)
+#         else:
+#             return jsonify({'status':'404'})
+
+# @app.route('/apartment/onwer/<id>',methods=['GET'])
+# def apartment_user_by_id(id):
+#         apartment = Apartment.query.filter_by(idonwer = id)
+#         result = apartments_schema.dump(apartment)
+#         if result:
+#             return jsonify(result)
+#         else:
+#             return jsonify({'status':'404'})
+
+# @app.route('/apartment/<id>',methods=['DELETE'])
+# def delete_apartment(id):
+#     try:  
+#         apartment = Apartment.query.get(id)
+#         db.session.delete(apartment)
+#         db.session.commit()
+#         return jsonify({'status':'200'})
+#     except:
+#         return jsonify({'status':'404'})
+
+# @app.route('/apartment', methods=['POST'])
+# def send_apartment():
+#     data = request.get_json(force=True)
+#     name = data['name']
+#     idonwer = data['idonwer']
+#     location = data['location']
+#     country = data['country']
+#     city = data['city']
+#     address = data['address']
+#     image = data['image']
+#     image_featured = data['image_featured']
+#     nigth_value = data['nigth_value']
+#     review = data['review']
+
+
+#     regist = Apartment(idonwer, name, city, address, location, country, image, image_featured,nigth_value,review)
+#     db.session.add(regist)
+#     db.session.commit()
+#     return jsonify({'status':'200'})
+
+# @app.route('/apartment/<id>', methods=['PUT'])
+# def up_apartment(id):
+#     try:
+#         apartment = Apartment.query.get(id)
+#         data = request.get_json(force=True)
+#         name = data['name']
+#         idonwer = data['idonwer']
+#         location = data['location']
+#         country = data['country']
+#         city = data['city']
+#         address = data['address']
+#         image = data['image']
+#         image_featured = data['image_featured']
+#         nigth_value = data['nigth_value']
+#         review = data['review']
+
+#         apartment.name = name
+#         apartment.idonwer = idonwer
+#         apartment.location = location
+#         apartment.country = country
+#         apartment.city = city
+#         apartment.address = address
+#         apartment.image = image
+#         apartment.image_featured = image_featured
+#         apartment.nigth_value = nigth_value
+#         apartment.review = review
+
+#         db.session.commit()
+#         return jsonify({'status':'200'})
+#     except:
+#         return jsonify({'status':'404'})
 
 
 
 
 if __name__ == '__main__':
     app.run(debug=True, port=8001)
-
