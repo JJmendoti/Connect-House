98dbc970baaf6cbf169326fed629a75f396158aa
Jhon Mendoza <jmendoti.cesde@gmail.com>
2021-04-20 19:53:44 -0500

    Jhon se actualiza a slider inicial

--- static/js/forms-control.js
+++ static/js/forms-control.js
@@ -28,63 +28,35 @@ $("li a").click(function (e) {
   $this.parent().addClass("active");
 });
 
-//matriz para contener las diferentes posiciones de la imagen
-let itemPositions = [];
-let numberOfItems = $('#scroller .item').length;
-// le asignamos a cada elemento de la matriz una clase CSS en función de su posición inicial
-function assignPositions() {
-  for (let i = 0; i < numberOfItems; i++) {
-    if (i === 0) {
-      itemPositions[i] = 'left-hidden';
-    } else if (i === 1) {
-      itemPositions[i] = 'left';
-    } else if (i === 2) {
-      itemPositions[i] = 'middle';
-    } else if (i === 3) {
-      itemPositions[i] = 'right';
-    } else {
-      itemPositions[i] = 'right-hidden';
-    }
-  }
-  //Se Agrega cada clase al elemento correspondiente * /
-  $('#scroller .item').each(function (index) {
-    $(this).addClass(itemPositions[index]);
+window.addEventListener("load", function () {
+  new Glider(document.querySelector(".carousel__lista"), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: ".carousel__indicadores",
+    arrows: {
+      prev: ".carousel__anterior",
+      next: ".carousel__siguiente",
+    },
+    responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 450,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        // screens greater than >= 1024px
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+        },
+      },
+    ],
   });
-}
-
-//cambiamos valores de la matriz y agregamos la clases a las imagenes
-function scroll(direction) {
-  if (direction === 'prev') {
-    itemPositions.push(itemPositions.shift());
-  } else if (direction === 'next') {
-    itemPositions.unshift(itemPositions.pop());
-  }
-  $('#scroller .item').removeClass('left-hidden left middle right right-hidden').each(function(index) {
-    $(this).addClass(itemPositions[index]);
-  });
-}
-
-// esto se hace cuando los elementos del DOM estén listos 
-$(document).ready(function() {
-
-  assignPositions();
-  let autoScroll = window.setInterval("scroll('next')", 3000);
-
-//Comportamientos del desplazamiento
-  $('#scroller').hover(function() {
-    window.clearInterval(autoScroll);
-    $('.nav').stop(true, true).fadeIn(200);
-  }, function() {
-    autoScroll = window.setInterval("scroll('next')", 3000);
-    $('.nav').stop(true, true).fadeOut(200);
-  });
-
-//Comportamientos de clics
-  $('.prev').click(function() {
-    scroll('prev');
-  });
-  $('.next').click(function() {
-    scroll('next');
-  });
-
 });
+
